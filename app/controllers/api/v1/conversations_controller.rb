0e4210de8c8127a023c2f5b956d49129ded3246a
hwangjf <jeffreyhwang90@gmail.com>
2019-09-09 16:07:50 -0400

    updates

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -8,7 +8,6 @@ class Api::V1::ConversationsController < ApplicationController
   def create
     conversation = Conversation.find_or_initialize_by(conversation_params)
     if conversation || conversation.save
-      
       # this is what sends the things involved over this channel
       ActionCable.server.broadcast 'conversation_channel', conversation
       render json: conversation
