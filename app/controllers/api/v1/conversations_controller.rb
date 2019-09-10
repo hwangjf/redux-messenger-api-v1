02334c1f577571819383dc7f4c0a74c4b95bc529
hwangjf <jeffreyhwang90@gmail.com>
2019-09-10 17:57:45 -0400

    many changes

--- app/controllers/api/v1/conversations_controller.rb
+++ app/controllers/api/v1/conversations_controller.rb
@@ -2,6 +2,7 @@ class Api::V1::ConversationsController < ApplicationController
   
   def index
     conversations = Conversation.all
+    
     render json: conversations
   end
 
--- app/serializers/message_serializer.rb
+++ app/serializers/message_serializer.rb
@@ -1,3 +1,7 @@
 class MessageSerializer < ActiveModel::Serializer
-  attributes :id, :conversation_id, :text, :created_at
+  attributes :id, :conversation_id, :text, :created_at, :user
+  # belongs_to :user, serializer: UserSerializer
+  def user
+    UserSerializer.new(object.user)
+  end
 end
