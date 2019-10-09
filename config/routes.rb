5bc493ff87597a8f4a71bb1ff994285e8501be77
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2019-10-09 00:23:07 -0400

    route

--- config/routes.rb
+++ config/routes.rb
@@ -2,7 +2,7 @@ Rails.application.routes.draw do
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 
   get '/', to: 'welcome#index'
-  
+  mount ActionCable.server => "/cable"
   namespace :api do
     namespace :v1 do
       resources :users
@@ -14,7 +14,6 @@ Rails.application.routes.draw do
       get '/auto_login', to: 'users#auto_login'
       post '/add_friend', to: 'users#add_friend'
 
-      mount ActionCable.server => '/cable'
     end
   end
 end
