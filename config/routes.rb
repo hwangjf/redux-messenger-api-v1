36a7054df8fef0724df56cef2076ddb28550f7af
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2019-10-10 01:10:44 -0400

    route edit

--- config/routes.rb
+++ config/routes.rb
@@ -3,19 +3,19 @@ Rails.application.routes.draw do
 
   get '/', to: 'welcome#index'
   mount ActionCable.server => "/cable"
+  
   namespace :api do
     namespace :v1 do
       resources :users, only: [:index, :update, :delete]
       resources :conversations, only: [:index, :create]
       resources :messages, only: [:create]
-
-      post '/signup', to: 'users#create'
-      post '/login', to: 'users#login' 
-      get '/auto_login', to: 'auth#auto_login'
-      post '/add_friend', to: 'users#add_friend'
       
       post '/users/:id/friends', to: 'users#add_friend'
       get '/users/:id/friends', to: 'users#friends'
+      
+      post '/signup', to: 'users#create'
+      post '/login', to: 'users#login' 
+      get '/auto_login', to: 'auth#auto_login'
 
     end
   end
