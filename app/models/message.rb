cfecc41f9c7eceec27e19a06f545748cd59cbd7e
Jeffrey Hwang <jeffreyhwang90@gmail.com>
2019-10-14 17:47:47 -0400

    fixed message after create hook

--- app/models/message.rb
+++ app/models/message.rb
@@ -5,8 +5,8 @@ class Message < ApplicationRecord
   after_create :create_participant
   
   def create_participant
-    if !Participant.where(user_id: self.user.id, conversation_id: self.conversation.id)
-      Participant.create(user_id: self.user.id, conversation_id: self.conversation.id)
+    if !Participant.exists?(user_id: user.id, conversation_id: conversation.id)
+      Participant.create(user_id: user.id, conversation_id: conversation.id)
     end
   end
 end
